5ba6098c1976e91a14395994354eab7050d8d28e
ganeshk2014 <35740048+ganeshk2014@users.noreply.github.com>
2020-03-19 19:02:51 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn seasoncal:app
---
+++ seasoncal.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+from flask import Flask
+# get the current day of the year
+from  datetime import date,datetime
+app=Flask(_name_)
+@app.route('/default')
+def default():
+    doy = datetime.today().timetuple().tm_yday
+
+    # "day of year" ranges for the northern hemisphere
+    #winter = range(, 59)
+    summer = range(60, 152)
+    fall = range(153, 305)
+    # winter = everything else
+    if doy in summer:
+        season = '1'
+    elif doy in fall:
+        season = '0'
+    else:
+        season = '2'
+    return season
+if __name__='__main__':
+    app.run(debug=True)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
